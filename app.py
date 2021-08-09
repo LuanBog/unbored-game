d12666ace81c69be3cb2df13768803416df32c83
LuanBog <mynameiscutepanda@gmail.com>
2021-08-09 08:49:40 +0800

    Added default Guest player

--- app.py
+++ app.py
@@ -3,25 +3,32 @@ import random
 import pickle
 import game_loader
 from form import Form
+from player import Player
 
 games = game_loader.load()
-
-score = {
-    'wins': 0,
-    'loses': 0
-}
-
 running = True
 
+guest = Player('Guest')
+current_player = guest
+players = [] # Guest will automatically added here with load_save()
+
+# This function is used to update the score when the current_player's score is updated. Because it doesn't naturally update when you update current_player's score
+# def update_players_score():
+#     global players
+
+#     for player in players:
+#         if player.name == current_player.name:
+#             player.score = current_player.score
+
 def play():
-    global score
+    global guest, current_player
 
     games_title = [game.title for game in games]
     games_title.append('Random')
     games_title.append('Back')
 
     print('')
-    print('Wins: {}, Loses: {}'.format(score['wins'], score['loses']))
+    print('Wins: {}, Loses: {}'.format(current_player.score['wins'], current_player.score['loses']))
 
     game_menu = Form('Games', games_title)
     game_menu_input = game_menu.ask()
@@ -47,44 +54,65 @@ def play():
 
     if game_chosen.won:
         print('Wow you won, GG\'s!\n')
-        score['wins'] += 1
+        current_player.score['wins'] += 1
     else:
         print('You lost, but you did your best\n')
-        score['loses'] += 1
+        current_player.score['loses'] += 1
+
+    # update_players_score()
 
     time.sleep(2)
 
 def quit():
     global running
 
-    print('\nPeace out!')
+    print('')
     save_save()
+    print('Peace out!')
     running = False
 
 def save_save():
+    global players
+
+    print('Saving...')
+
     with open('save.db', 'wb') as save:
-        pickle.dump(score, save)
+        # Removes guest from players, so it doesn't get saved
+        for player in players:
+            if player.name == 'Guest':
+                players.remove(player)
+
+        pickle.dump(players, save)
 
 def load_save():
+    global players
+
+    save = None
+
     try:
         with open('save.db', 'rb') as save:
-            return pickle.load(save)
+            save = pickle.load(save)
     except FileNotFoundError:
         print('\nSave file isn\'t found! Creating...\n')
         save_save()
 
         with open('save.db', 'rb') as save:
-            return pickle.load(save)
+            save = pickle.load(save)
+
+    players = save
+    players.insert(0, guest) # Guest should always be at the beginning of the list
 
 def main():
     global running
 
     print('\nAre you bored and ready to play?')
     time.sleep(2)
-    print('\nI welcome you to Unbored Game!\nA game that makes you unbored')
+    print('\nI welcome you to Unbored Game!\nA game that makes you unbored\n')
     time.sleep(2)
     
     while running:
+        print('Player: {}'.format(current_player.name))
+
         menu = Form('Menu', ['Play', 'About', 'Quit'])
         menu_input = menu.ask()
 
@@ -103,10 +131,7 @@ def main():
             time.sleep(0.5)
 
 if __name__ == '__main__':
-    save = load_save()
-    
-    if save:
-        score = save
+    load_save()
 
     try:
         main()
---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = {
+            'wins': 0,
+            'loses': 0
+        }
